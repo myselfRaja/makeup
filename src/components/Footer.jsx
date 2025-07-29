36ad7b851acdc19dbbe89ae5b5331d1ae59caeff
Mohammed Raza <mohammedraza11239@gmail.com>
2025-07-29 11:29:19 +0530

    fixing name

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -37,7 +37,7 @@ const Footer = () => {
 
       {/* Bottom */}
       <div className="mt-10 border-t border-[#eee] pt-6 text-center text-sm text-[#777]">
-        &copy; {new Date().getFullYear()} Glam By [YourName]. All rights reserved.
+        &copy; {new Date().getFullYear()} Glam By Sakti. All rights reserved.
       </div>
     </footer>
   );
